2b8baf8fea824e57be30d888bfb58b02a33046fb
PupS1k <denikpro@mail.ru>
2019-09-23 21:25:20 +0300

    install react-time-ago and use in ProductCard

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import JavascriptTimeAgo from 'javascript-time-ago';
+import ru from 'javascript-time-ago/locale/ru';
 import App from './components/App';
 import store from './store';
+
+
+JavascriptTimeAgo.locale(ru);
 
 ReactDOM.render(
   <Provider store={store}>
