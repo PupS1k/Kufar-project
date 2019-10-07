7d6de1bf009930d4a1ee66530534113e12cf31cc
PupS1k <denikpro@mail.ru>
2019-10-07 23:38:06 +0300

    add portal for modal window

--- src/components/ModalWindow/index.js
+++ src/components/ModalWindow/index.js
@@ -1,25 +1,31 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import IconButton from '../IconButton';
 import Authorization from '../Authorization';
 import close from '../../images/plusReset.png';
 import './style.css';
 
 const ModalWindow = props => (
-  <div className="modal">
-    <div className="modal__items">
-      <div className="modal__close">
-        <IconButton
-          className="modal__close-button"
-          onClick={props.toggleIsOpenModel}
-          image={{
-            alt: 'Close',
-            icon: close,
-            iconSize: 'medium'
-          }}
-        />
-      </div>
-      <Authorization />
-    </div>
+  <div>
+    {ReactDOM.createPortal(
+      <div className="modal">
+        <div className="modal__items">
+          <div className="modal__close">
+            <IconButton
+              className="modal__close-button"
+              onClick={props.toggleIsOpenModel}
+              image={{
+                alt: 'Close',
+                icon: close,
+                iconSize: 'medium'
+              }}
+            />
+          </div>
+          <Authorization />
+        </div>
+      </div>,
+      document.getElementById('portal')
+    )}
   </div>
 );
 
