755718e17e1af2c7e33eac586e1e553f82b762ee
PupS1k <denikpro@mail.ru>
2019-10-11 12:45:26 +0300

    create tests for Button component

--- src/components/Button/test.js
+++ src/components/Button/test.js
@@ -1,25 +1,77 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import Button from './index';
 
 
-describe('News container', () => {
+describe('Button container', () => {
   const props = {
-    label: '',
-    mode: '',
-    labelSize: '',
+    label: 'label',
+    mode: 'mode',
+    labelSize: 'labelSize',
     type: 'button',
     className: '',
     bold: false,
     disabled: false,
-    onClick: () => {},
+    onClick: jest.fn(),
     image: null
   };
 
-  const component = shallow(<Button {...props}/>);
+  describe('Button initial', () => {
+    const image = {
+      iconSize: 'size',
+      icon: 'icon',
+      alt: 'alt'
+    };
+    const component = shallow(<Button {...props} image={image} />);
 
+    test('render button', () => {
+      expect(component.find('button')).toHaveLength(1)
+    });
 
-  test('no render Button', () => {
-    expect(component.find(Button)).toHaveLength(0)
+    test('render text in button', () => {
+      expect(component.find('p')).toHaveLength(1)
+    });
+
+    test('render image in button', () => {
+      expect(component.find('img')).toHaveLength(1)
+    });
+  });
+
+  describe('render button', () => {
+    const newProps = {
+      ...props,
+      label: 'text'
+    };
+
+    const component = shallow(<Button {...newProps}/>);
+
+    test('render text in button', () => {
+      expect(component.find('p').text()).toEqual(newProps.label)
+    })
+  });
+
+  describe('require variable', () => {
+    const component = mount(<Button {...props} />);
+    test('label is not undefined', () => {
+      expect(component.props().label).not.toBeUndefined()
+    });
+
+    test('labelSize is not undefined', () => {
+      expect(component.props().labelSize).not.toBeUndefined()
+    });
+
+    test('mode is not undefined', () => {
+      expect(component.props().mode).not.toBeUndefined()
+    });
+  });
+
+  describe('when clicking the button', () => {
+    const component = shallow(<Button {...props} />);
+
+    component.find('button').simulate('click');
+
+    test('click the button', () => {
+      expect(props.onClick).toHaveBeenCalled()
+    })
   });
 });
