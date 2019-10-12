e7f46c43f157556a1da692cac045dafbe2bf0c55
PupS1k <denikpro@mail.ru>
2019-10-12 14:20:45 +0300

    create test for CheckboxList

--- src/components/CheckboxList/index.js
+++ src/components/CheckboxList/index.js
@@ -1,37 +1,31 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import {guid} from '../../utils';
 import './style.css';
 
-class CheckboxList extends PureComponent {
-
-  render() {
-    const {
-      headline, classNameFilter, isChecked,
-      filters, handlesSwitchFilter
-    } = this.props;
-    return (
-      <div>
-        {headline && <p className="label-input">{headline}</p>}
-        <div className={classNameFilter}>
-          {filters.map((filter, index) => (
-            <label key={guid()} className="icon-label">
-              <input
-                className="icon-input"
-                type="checkbox"
-                value={filter.name}
-                onChange={handlesSwitchFilter[index]}
-                name={filter.name}
-                checked={isChecked[index]}
-              />
-              <div className="icon-checkbox-btn" />
-              <p>{filter.name}</p>
-            </label>
-          ))}
-        </div>
-      </div>
-    )
-  }
-}
+const CheckboxList = ({
+  headline, classNameFilter, isChecked,
+  filters, handlesSwitchFilter
+}) => (
+  <div>
+    {headline && <p className="label-input">{headline}</p>}
+    <div className={classNameFilter}>
+      {filters.map((filter, index) => (
+        <label key={guid()} className="icon-label">
+          <input
+            className="icon-input"
+            type="checkbox"
+            value={filter.name}
+            onChange={handlesSwitchFilter[index]}
+            name={filter.name}
+            checked={isChecked[index]}
+          />
+          <div className="icon-checkbox-btn"/>
+          <p>{filter.name}</p>
+        </label>
+      ))}
+    </div>
+  </div>
+);
 
 
 export default CheckboxList;
---
+++ src/components/CheckboxList/test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import CheckboxList from './index';
+
+describe('CheckboxList container', () => {
+  const props = {
+    headline: 'headline',
+    classNameFilter: 'className',
+    filters: [213,123,21,3],
+    handlesSwitchFilter: [],
+    isChecked: []
+  };
+
+  describe('CheckboxList init', () => {
+    const component = shallow(<CheckboxList {...props} />);
+
+    test('checkbox render', () => {
+      expect(component.find('input')).toHaveLength(props.filters.length)
+    });
+
+    test('label render', () => {
+      expect(component.find('label')).toHaveLength(props.filters.length)
+    });
+
+    test('headline render', () => {
+      expect(component.find('.label-input')).toHaveLength(1)
+    });
+
+    test('paragraph render', () => {
+      expect(component.find('label p')).toHaveLength(props.filters.length)
+    });
+  });
+  describe('render text in field', () => {
+    const component = shallow(<CheckboxList {...props} />);
+
+    test('render text in headline', () => {
+      expect(component.find('.label-input').text()).toHaveLength(props.headline)
+    });
+
+  });
+});
